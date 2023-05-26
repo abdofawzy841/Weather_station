65ef2816678e36d40fc814eef53cbf83bcdec2f0
AbdelrhmanKassem <bodyel92@yahoo.com>
2023-05-26 21:18:54 +0300

    cleanup message producers

---
+++ weather-station/src/main/java/org/example/Station.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.*;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+
+public class Station extends Thread {
+
+  private KafkaProducer<String,String> producer;
+  private MessageGenerator messageGenerator;
+  private String topicName;
+  private Random droppingRandom;
+
+  public Station(long stationID, long startingSequenceNumber, Properties producerProps, String topicName) {
+    this.messageGenerator = new MessageGenerator(stationID, startingSequenceNumber);    
+    this.producer = new KafkaProducer<>(producerProps);
+    this.topicName  = topicName;
+    this.droppingRandom = new Random();
+  }
+
+  public void run() {
+    while (true) {
+      long startTime = System.currentTimeMillis();
+      String message = messageGenerator.getNextMessage();
+      if (droppingRandom.nextDouble() > 0.1 ) {
+        producer.send(new ProducerRecord<String,String>(topicName, message));
+      }
+      long timeTaken = System.currentTimeMillis() - startTime;
+      System.out.println("Sending message took " + timeTaken + " ms");
+      System.out.println("Message: " + message);
+      try {
+        Thread.sleep((1000 - timeTaken) > 0 ? 1000 - timeTaken : 0);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+}
