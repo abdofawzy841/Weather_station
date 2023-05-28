4ebc00cb94cecae72dc549da69a86685dc931a12
AbdelrhmanKassem <bodyel92@yahoo.com>
2023-05-28 21:57:41 +0300

    edit rain trigger bootstrap values

--- weather-station/src/main/java/org/example/RainDetectionProcessor.java
+++ weather-station/src/main/java/org/example/RainDetectionProcessor.java
@@ -19,7 +19,7 @@ public class RainDetectionProcessor {
         // Set up Kafka Streams configuration
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "rain-detection-processor");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
